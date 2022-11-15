69e39975e89cd5f4a537bb3c088b90f300c894cf
chandrabhansolanki <cbss1994@gmail.com>
2022-11-16 00:25:21 +0530

    new mongo url

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 
-const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
+// const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true"
+
+const mongoURI = "mongodb+srv://chandra:1994%40Bhan@cluster0.fqjjcv1.mongodb.net/Company?retryWrites=true&w=majority"
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI,()=> {
